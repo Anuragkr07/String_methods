04fdc6e7fd057cb5374785dc227245fdebd8df59
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-04-11 07:54:41 +0530

    Create trim a string.cpp

---
+++ trim a string.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeSpaces(string str)
+{
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    return str;
+}
+
+int main()
+{
+    
+    string s = " hello world ";
+    s=removeSpaces(s);
+    cout<<s;
+
+    return 0;
+}
+
+// Op:helloworld
+
+
+
+/*
+
+txt = "     ban ana     "
+txt = txt.strip()
+print(txt)
+
+
+O/p:banana
+
+*/
