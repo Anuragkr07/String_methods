c8aa17884f423599f9afbeb99289d04c75fd835b
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-04-11 08:12:09 +0530

    Create startsWith a prefix.cpp

---
+++ startsWith a prefix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool startsWith(string mainStr, string toMatch)
+{
+    if(mainStr.find(toMatch) == 0)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    string str;
+    str = "hey how are you";
+    if(startsWith(str,"hei"))
+        cout<<"true";
+    else
+        cout<<"false";
+    
+    
+    return 0;
+}
